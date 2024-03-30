857c6183e750726815fb225f80bf1e925d099a21
LJ <2335404839@qq.com>
2024-03-30 23:02:57 +0800

    bean后处理器

--- show/src/main/java/com/itheima/a04/A04.java
+++ show/src/main/java/com/itheima/a04/A04.java
@@ -21,12 +21,12 @@ public class A04 {
         context.registerBean("bean3", Bean3.class);
         context.registerBean("bean4", Bean4.class);
 
-        context.getDefaultListableBeanFactory().setAutowireCandidateResolver(new ContextAnnotationAutowireCandidateResolver());
-        context.registerBean(AutowiredAnnotationBeanPostProcessor.class); // @Autowired @Value
+        context.getDefaultListableBeanFactory().setAutowireCandidateResolver(new ContextAnnotationAutowireCandidateResolver());// 负责获取 @Value 的值，解析 @Qualifier、泛型、@Lazy 等
+        context.registerBean(AutowiredAnnotationBeanPostProcessor.class); // 解析 @Autowired @Value(依赖注入阶段)
 
-        context.registerBean(CommonAnnotationBeanPostProcessor.class); // @Resource @PostConstruct @PreDestroy
+        context.registerBean(CommonAnnotationBeanPostProcessor.class); // @Resource (依赖注入阶段) @PostConstruct (初始化前) @PreDestroy (销毁前)
 
-        ConfigurationPropertiesBindingPostProcessor.register(context.getDefaultListableBeanFactory());
+        ConfigurationPropertiesBindingPostProcessor.register(context.getDefaultListableBeanFactory()); // ConfigurationProperties (初始化前)
 
         // ⬇️初始化容器
         context.refresh(); // 执行beanFactory后处理器, 添加bean后处理器, 初始化所有单例
